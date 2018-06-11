f1c9e5f29f797b662b1d6525af140a5793a454dc
James Tapsell <email@jrtapsell.co.uk>
2018-06-11 09:43:52 +0100

    Removed commented out lines

--- standup.sh
+++ standup.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# https://askubuntu.com/questions/15853/how-can-a-script-check-if-its-being-run-as-root
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
 pushd .
 
 docker-compose -f mongo-sharded/replica.yml stop
 docker-compose -f mongo-sharded/replica.yml rm -f
-rm -rf mongo-sharded/data
 
 docker-compose -f mongo-sharded/replica.yml up --no-start
 docker-compose -f mongo-sharded/replica.yml start
