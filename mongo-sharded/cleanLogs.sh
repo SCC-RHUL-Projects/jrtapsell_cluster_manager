5786bfb8c7abd4a108ac0a770cf2d4b83ee3c06c
James Tapsell <email@jrtapsell.co.uk>
2018-07-15 16:55:47 +0100

    Added logging for stderr

--- mongo-sharded/cleanLogs.sh
+++ mongo-sharded/cleanLogs.sh
@@ -17,20 +17,35 @@ touch mongo_config3.prov
 touch mongos1.prov
 touch mongos2.prov
 
-touch mongo_shard1_node1.std
-touch mongo_shard1_node2.std
-touch mongo_shard1_node3.std
+touch mongo_shard1_node1.stdout
+touch mongo_shard1_node2.stdout
+touch mongo_shard1_node3.stdout
 
-touch mongo_shard2_node1.std
-touch mongo_shard2_node2.std
-touch mongo_shard2_node3.std
+touch mongo_shard2_node1.stdout
+touch mongo_shard2_node2.stdout
+touch mongo_shard2_node3.stdout
 
-touch mongo_config1.std
-touch mongo_config2.std
-touch mongo_config3.std
+touch mongo_config1.stdout
+touch mongo_config2.stdout
+touch mongo_config3.stdout
 
-touch mongos1.std
-touch mongos2.std
+touch mongos1.stdout
+touch mongos2.stdout
+
+touch mongo_shard1_node1.stderr
+touch mongo_shard1_node2.stderr
+touch mongo_shard1_node3.stderr
+
+touch mongo_shard2_node1.stderr
+touch mongo_shard2_node2.stderr
+touch mongo_shard2_node3.stderr
+
+touch mongo_config1.stderr
+touch mongo_config2.stderr
+touch mongo_config3.stderr
+
+touch mongos1.stderr
+touch mongos2.stderr
 
 chmod 666 ./*
 
