aefcdbb439e1d57afe29df229f70b337d8b879a5
James Tapsell <email@jrtapsell.co.uk>
2018-07-15 16:09:43 +0100

    Fixed issue with docker volumes

--- mongo-sharded/cleanLogs.sh
+++ mongo-sharded/cleanLogs.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 rm -rf logs
 mkdir logs
+cd logs
+touch mongo_shard1_node1.prov
+touch mongo_shard1_node2.prov
+touch mongo_shard1_node3.prov
+
+touch mongo_shard2_node1.prov
+touch mongo_shard2_node2.prov
+touch mongo_shard2_node3.prov
+
+touch mongo_config1.prov
+touch mongo_config2.prov
+touch mongo_config3.prov
+
+touch mongos1.prov
+touch mongos2.prov
+
+touch mongo_shard1_node1.std
+touch mongo_shard1_node2.std
+touch mongo_shard1_node3.std
+
+touch mongo_shard2_node1.std
+touch mongo_shard2_node2.std
+touch mongo_shard2_node3.std
+
+touch mongo_config1.std
+touch mongo_config2.std
+touch mongo_config3.std
+
+touch mongos1.std
+touch mongos2.std
+
+chmod 666 ./*
+
