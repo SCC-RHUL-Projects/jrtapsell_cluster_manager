9971daa24441ed4ef993d0c5037cff9cc78bb898
James Tapsell <email@jrtapsell.co.uk>
2018-07-19 12:25:27 +0100

    Updated to bulk insert

--- inserter/index.js
+++ inserter/index.js
@@ -1,13 +1,24 @@
 const {MongoClient} = require("mongodb");
+const _ = require("lodash");
+const fs = require("fs");
+const {promisify} = require("util");
+
+const readPromise = promisify(fs.readFile);
 
 async function main() {
+    const text = (await readPromise("./lorum.txt")).toString();
     const client = await MongoClient.connect("mongodb://localhost:27017,localhost:27018",{ useNewUrlParser: true });
     const collection = client
         .db("testDb")
         .collection("testCollection");
-    const ins = await collection.insert({
-        "Hello": "World"
-    });
+
+    await Promise.all(_.map(_.range(100), async (value) => {
+        const ins = await collection.insert({
+            "text": "Hello World",
+            "count": value,
+            "bloat": text
+        });
+    }));
     await client.close();
 }
 
