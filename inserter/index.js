feb2279d16a423a76ac9fac1cda436e3f530e383
James Tapsell <email@jrtapsell.co.uk>
2018-07-19 12:13:05 +0100

    Trying to open connection

--- inserter/index.js
+++ inserter/index.js
@@ -1,13 +1,15 @@
 const {MongoClient} = require("mongodb");
 
 async function main() {
-    const client = await MongoClient("mongodb://localhost:27017,localhost:27018");
+    const client = new MongoClient("mongodb://localhost:27017,localhost:27018");
+    await client.open();
     const collection = client
         .db("testDb")
         .collection("chunks");
     const ins = await collection.insert({
         "Hello": "World"
-    })
+    });
+    await client.close();
 }
 
 main();
