5caaa8e07bb79ce5f0eb1d76d22f9748323f915d
James Tapsell <email@jrtapsell.co.uk>
2018-07-19 12:10:58 +0100

    Updated to insert data

--- inserter/index.js
+++ inserter/index.js
@@ -2,6 +2,12 @@ const {MongoClient} = require("mongodb");
 
 async function main() {
     const client = await MongoClient("mongodb://localhost:27017,localhost:27018");
+    const collection = client
+        .db("testDb")
+        .collection("chunks");
+    const ins = await collection.insert({
+        "Hello": "World"
+    })
 }
 
-main()
+main();
