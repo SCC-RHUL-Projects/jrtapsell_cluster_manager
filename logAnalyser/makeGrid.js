0f565f2f95fcf7f9ce14242d33aa976c157d4d78
James Tapsell <email@jrtapsell.co.uk>
2018-07-19 13:34:17 +0100

    Reworked the log analyser

--- logAnalyser/makeGrid.js
+++ logAnalyser/makeGrid.js
@@ -1,40 +1,27 @@
-const containers = require("./containers.json");
-const events = require("./events.json");
-const _ = require("lodash");
+const LOG_DIR = "/home/james/tmp/logs/files";
+
 const fs = require("fs");
+const {promisify} = require("util");
+const _ = require("lodash");
 
-const data = _.chain(containers)
-    .map(p => {
-        const q = fs.readFileSync(`mongo-sharded/logs/${p}`).toString();
-        return [p, _.chain(q.split("\n"))
-            .filter(p => p !== "")
-            .map(p => JSON.parse(p))
-            .map(p => {
-                if (p.event === "logCommandAuthzCheck") {
-                    return "C_" + p.payload;
-                } else {
-                    return p.event;
-                }
-            })
-            .uniqBy()
-            .value()]
-    })
-    .value();
+const ls = promisify(fs.readdir);
+const read = (p) => promisify(fs.readFile)(p).then(q => q.toString());
 
-const allEvents = _.chain(data)
-    .flatMap(p => p[1])
-    .uniqBy()
-    .value();
+async function main() {
+    const servers = _.chain(await ls(LOG_DIR))
+        .filter(p => p.endsWith(".prov"))
+        .map(p => p.substr(0, p.length - 5))
+        .value();
 
-const servers = _.chain(data)
-    .groupBy(p => p[0])
-    .mapValues(p => p[0][1])
-    .mapValues((v) => v.map(p => allEvents.indexOf(p)))
-    .value();
+    const rawData = await Promise.all(_.map(servers, p => read(`${LOG_DIR}/${p}.prov`)));
+    const lines = rawData.map(p => p.split("\n").map(q => {
+        try {
+            return JSON.parse(q)
+        } catch (e) {
+            console.log(e);
+        }
+    }));
+    console.log(lines)
+}
 
-const header = "," + allEvents.join(",");
-
-const body = _.map(servers, (k, v) => v + "," + _.chain(_.range(allEvents.length).map(q => k.indexOf(q) !== -1)).value()).join("\n")
-//console.log(header);
-
-fs.writeFileSync("results.csv", header + "\n" + body);
+main();
